293804ba1de9e854b8cbf8e9533f04514f6b5cd2
Ethan Winegardner <ejosephhiii@gmail.com>
2022-08-01 10:17:21 -0600

    done

--- Server/homeback.js
+++ Server/homeback.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+require('dotenv').config()
 
 const { postKayak, deleteKayak } = require("./controller");
 
 const app = express();
+
+app.get ('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, '../home.css'))
+})
+
+app.get ('/styles2', (req, res) => {
+    res.sendFile(path.join(__dirname, '../list.css'))
+})
+
+app.get ('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../homefront.js'))
+})
+
+app.get ('/js2', (req, res) => {
+    res.sendFile(path.join(__dirname, '../listfront.css'))
+})
+
+
 
 app.use(cors());
 
